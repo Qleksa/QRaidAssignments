4a070f16a41fe50150e7638eadfe9d3f0380fcfb
Qleksa <qleksa@gmail.com>
2026-06-10 13:01:39 +0200

    chore: update interface version

--- Init.lua
+++ Init.lua
@@ -8,7 +8,7 @@ local QRA = select(2, ...)
 QRA = QRA or {}
 
 QRA.name = "QRaidAssignments"
-QRA.version = "2.1.0"
+QRA.version = "2.1.1"
 
 --------------------------------------------------
 -- Pre-initialize module tables
