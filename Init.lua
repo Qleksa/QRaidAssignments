c47fdc704ecf472245a35e4c07521de4e3b0f120
Qleksa <qleksa@gmail.com>
2026-06-15 16:30:15 +0200

    chore: bump version

--- Init.lua
+++ Init.lua
@@ -8,7 +8,7 @@ local QRA = select(2, ...)
 QRA = QRA or {}
 
 QRA.name = "QRaidAssignments"
-QRA.version = "2.2.0"
+QRA.version = "2.2.1"
 
 --------------------------------------------------
 -- Pre-initialize module tables
